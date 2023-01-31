cee761018e617fc5e729c0891cfa6c499ebbb705
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2023-01-31 18:51:40 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -6,12 +6,17 @@ $app = new Engine();
 // autoload controllers ////////
 $app->path('controllers');
 
-// controllers for register ////
+// view controllers for register ////
 $controllers = [
     'display' => 'ViewController'
 ];
 
-// register controllers ////////
+// register view controllers ////////
 foreach($controllers as $controller_name => $controller) {
     $app->register($controller_name, $controller);
 }
+
+// before start ///////////
+$app->before('start', function(){
+    
+});
