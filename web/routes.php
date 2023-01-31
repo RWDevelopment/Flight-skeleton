6c4ffea73073f1d52d0741dceabf4f72a03fde29
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2023-01-31 18:27:35 +0100

    Update routes.php

--- web/routes.php
+++ web/routes.php
@@ -1,1 +1,9 @@
+<?php
 
+// routes //////////////////
+$app->route('/', [ $app->display(), 'home' ]);
+$app->route('/about', [ $app->display(), 'about' ]);
+$app->route('/api', [ $app->api(), 'render_json' ]);
+
+// 404 error ///////////////
+$app->map('notFound', [ $app->display(), 'e_404' ]);
