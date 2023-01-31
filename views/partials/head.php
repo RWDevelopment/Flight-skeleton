8486b5c14e65b5623abd2302d4b305a7fc47383f
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2023-01-31 18:42:41 +0100

    Update head.php

--- views/partials/head.php
+++ views/partials/head.php
@@ -1,1 +1,8 @@
-
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css">
+    <title>Hello, world!</title>
+  </head>
