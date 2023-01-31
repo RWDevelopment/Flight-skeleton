093ae927790cfcda7d360d9c505efb50be42a093
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2023-01-31 18:30:39 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,3 +3,15 @@
 use flight\Engine;
 $app = new Engine();
 
+// autoload controllers ////////
+$app->path('controllers');
+
+// controllers for register ////
+$classes = [
+    'display' => 'ViewController'
+];
+
+// register controllers ////////
+foreach($classes as $class_name => $class_controller) {
+    $app->register($class_name, $class_controller);
+}
