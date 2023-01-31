4001e4cd8370bc1934c91feb7bb7118aa8b4d2a8
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2023-01-31 18:37:48 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -7,11 +7,11 @@ $app = new Engine();
 $app->path('controllers');
 
 // controllers for register ////
-$classes = [
+$controllers = [
     'display' => 'ViewController'
 ];
 
 // register controllers ////////
-foreach($classes as $class_name => $class_controller) {
-    $app->register($class_name, $class_controller);
+foreach($controllers as $controller_name => $controller) {
+    $app->register($controller_name, $controller);
 }
