0e7aa8c725a4d8e07491d682c4b9ca314750c94c
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2023-02-02 13:05:40 +0100

    Update head.php

--- views/partials/head.php
+++ views/partials/head.php
@@ -4,6 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css">
-    <title><?= $site_title; ?> | <?= $title; ?></title>
+    <title><?= $site_title; ?> | <?= $page_title; ?></title>
   </head>
 <body>
