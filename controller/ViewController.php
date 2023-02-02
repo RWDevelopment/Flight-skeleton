d28517c5430cf0497c7594019af50b6b14e1434d
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2023-02-02 12:23:30 +0100

    Update ViewController.php

--- controller/ViewController.php
+++ controller/ViewController.php
@@ -4,34 +4,34 @@ class ViewController {
 
   public function  __construct()  {
         
-        global $app;
-        $this->ctrl = $app;
+       global $app;
+       $this->ctrl = $app;
     
   }
   
   public function renderTemplate(string $template = null, array $page_data = null) {
 
-        $this->ctrl->response()->status(200)->header('content-type', 'text/html; charset=utf-8')->send();
-        $this->ctrl->render($template, $page_data);
+       $this->ctrl->response()->status(200)->header('content-type', 'text/html; charset=utf-8')->send();
+       $this->ctrl->render($template, $page_data);
 
   }
   
   public function home() {
 
-         $this->renderTemplate('home');
+       $this->renderTemplate('home');
         
   }
 
   public function about() {
 
-         $this->renderTemplate('about');
+        $this->renderTemplate('about');
     
   }
 
   public function e404() {
 
-         $this->ctrl->response()->status(404)->send();
-         $this->renderTemplate('404');
+        $this->ctrl->response()->status(404)->send();
+        $this->renderTemplate('404');
         
   }
   
