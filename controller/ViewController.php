d6ac1d6f52818e9868cf772eaa663e290f9518e9
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2023-02-02 12:51:56 +0100

    Update ViewController.php

--- controller/ViewController.php
+++ controller/ViewController.php
@@ -10,9 +10,28 @@ class ViewController {
   }
   
   public function renderTemplate(string $template = null, array $page_data = null) {
+    
+        $file = __DIR__.'/../pages/'.$file_data.'.md';
+    
+        if(file_exists($file)) {
+          
+            $page = Yaml::parseFile($file);
+          
+            $page_data = [
+              
+                'title' => $page->title,
+                'content' => $page->body()
+              
+            ];
+          
+            $this->ctrl->response()->status(200)->header('content-type', 'text/html; charset=utf-8')->send();
+            $this->ctrl->render($template, $page_data);
 
-       $this->ctrl->response()->status(200)->header('content-type', 'text/html; charset=utf-8')->send();
-       $this->ctrl->render($template, $page_data);
+        } else { 
+          
+            echo 'no data or templatefile';
+          
+        }
 
   }
   
